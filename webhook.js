7d98ed6eaa732b6e0cc70d31b00723c42028a83b
real-na <naogko@163.com>
2022-08-19 16:37:51 +0800

    testwebhook

--- webhook.js
+++ webhook.js
@@ -1,13 +1,12 @@
-const { read } = require('fs');
 let http = require('http');
 let server = http.createServer((req, res) => {
   console.log('zeenunew', req.method, req.url);
-  if (req.method === 'POST' && req.url === '/webhook') {
+  if (req.method == 'POST' && req.url == '/webhook') {
     // 给github服务器发送一个回应，响应体是json格式
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ ok: true }));
   } else {
-    res.end('Not Found')
+    res.end('Not Found1')
   }
 });
 
